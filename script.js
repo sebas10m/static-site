7bba770c7d27c7e7a2e3f6967b5400c454c23890
John <mymail>
2024-11-24 13:42:05 +0100

    Add snake game

--- script.js
+++
@@ -1,5 +0,0 @@
-// Basic interactivity
-document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Thank you for reaching out! We will get back to you soon.');
-});
